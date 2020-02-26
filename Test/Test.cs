a04fb303cf1ffc9f8420b40edfaad499907e607b
Adi Šoše <adi.sose@edu.fit.ba>
2020-02-26 20:30:22 +0100

    C++ and C# test files

---
+++ Test/Test.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
